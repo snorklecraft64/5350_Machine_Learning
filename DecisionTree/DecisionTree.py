753091d66aa8dcfe1add51bc05d9373a586cbe10
Erik Warling <snorklecraft@gmail.com>
2021-10-20 22:17:45 -0600

    Moved stuff into a Basics directory
    So multiple future apps can use it. All widely used features will go here

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,68 +1,8 @@
 import sys
+sys.path.append('../')
 import math
 import statistics
-
-##represents a single example in a training data set
-class Example:
-  def __init__(this, ID, weight, attributes, label):
-    this.__ID = ID
-    #the weight of this example
-    this.__weight = weight
-    #the attributes of this example (dictionary where keys are attributes and values are the value of that attribute)
-    this.__attributes = attributes
-    #the label given to this example
-    this.__label = label
-  
-  def __str__(this):
-    return '[' + str(this.__ID) + ', ' + str(this.__weight) + ' ' + str(this.__attributes) + ', ' + str(this.__label) + ']'
-  
-  def __repr__(this):
-    return this.__str__()
-  
-  def __hash__(this):
-    return this.__ID
-
-  def __eq__(this, other):
-    if this.__ID == other.__ID:
-      return true
-    return false
-  
-  def getID(this):
-    return this.__ID
-  def getAttrs(this):
-    return this.__attributes
-  def getLabel(this):
-    return this.__label
-  def getWeight(this):
-    return this.__weight
-
-##represents a single node in a tree
-class Node:
-  ##creates node
-  ##input:  isLeaf: if true, node will only have a label
-  ##                if false, node will have attribute and dict of child nodes
-  def __init__(this, attr, label):
-      #the label this node has
-      this.__label = label
-      #dictionary that stores the child nodes of this node
-      #the key is the value of the attribute, the value is the child node
-      this.__childs = {}
-      #the attribute this node splits by
-      this.__attr = attr
-  
-  ##add a given node with given attribute value as a child of this node
-  def add_child(this, c_node, a_value):
-    this.__childs[a_value] = c_node
-  
-  ##returns the child node for the specific value given
-  def get_child(this, a_value):
-    return this.__childs[a_value]
-  
-  def getLabel(this):
-    return this.__label
-  
-  def getAttr(this):
-    return this.__attr
+from Basics.Basics import *
 
 ##represents a decision tree, initially untrained
 class DecisionTree:
